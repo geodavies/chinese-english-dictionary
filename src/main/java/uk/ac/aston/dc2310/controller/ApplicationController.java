7ad8f9ab0a191cfc532cdb03a614e1f85002e957
George Davies <george@debian>
2017-05-20 12:29:49 +0100

    TG-5 Implemented OutputFormatter

--- src/main/java/uk/ac/aston/dc2310/controller/ApplicationController.java
+++ src/main/java/uk/ac/aston/dc2310/controller/ApplicationController.java
@@ -1,12 +1,7 @@
 package uk.ac.aston.dc2310.controller;
 
 import uk.ac.aston.dc2310.formatter.OutputFormatter;
-import uk.ac.aston.dc2310.model.Definition;
 import uk.ac.aston.dc2310.model.Definitions;
-import uk.ac.aston.dc2310.util.DictionaryParser;
-
-import java.io.IOException;
-import java.util.List;
 
 /**
  * @author George Davies
